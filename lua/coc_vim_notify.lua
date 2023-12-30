13ddcb98dd27654b4d496e0e7110553c63e45338
statiolake <statiolake@gmail.com>
2023-12-31 03:28:04 +0900

    fix: fallback to print() before coc.nvim initialized

--- lua/coc_vim_notify.lua
+++ lua/coc_vim_notify.lua
@@ -1,8 +1,13 @@
 local M = {}
 
 function M.notify(message, level)
-	level = level or "info"
-	vim.fn.CocAction("runCommand", "coc-vim-notify.showMessage", message, level)
+	if vim.g.coc_service_initialized == 1 then
+		level = level or 2
+		vim.fn.CocAction("runCommand", "coc-vim-notify.showMessage", message, level)
+	else
+		-- fall back to print()
+		print(message)
+	end
 end
 
 return M
